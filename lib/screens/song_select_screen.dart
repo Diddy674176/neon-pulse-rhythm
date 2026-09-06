68b06f1b57234dead515484c1f02dbd5059272c0
Diddy674176 <3xkckml@gmail.com>
2026-09-05 23:59:18 -0700

    DO NOT USE

--- lib/screens/song_select_screen.dart
+++ lib/screens/song_select_screen.dart
@@ -1,18 +1,1 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import '../app_state.dart';
-import '../song/song_catalog.dart';
-import '../ui/neon_widgets.dart';
-import '../vfx/neon_palette.dart';
-import 'gameplay_screen.dart';
-import 'import_song_screen.dart';
-
-part 'song_select_screen_state.dart';
-
-class SongSelectScreen extends StatefulWidget {
-  const SongSelectScreen({super.key, required this.practice});
-  final bool practice;
-
-  @override
-  State<SongSelectScreen> createState() => _SongSelectScreenState();
-}
+PLACEHOLDER_WILL_FAIL_BUILD
