75ab7f716d2e00a7da4d02fdcd4414337d1cb804
Diddy674176 <3xkckml@gmail.com>
2026-09-06 17:58:42 -0700

    fix(gameplay): import foundation for kIsWeb

--- lib/screens/gameplay_screen.dart
+++ lib/screens/gameplay_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/scheduler.dart';
